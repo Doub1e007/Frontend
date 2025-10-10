64f3f07c8950f8239cf2d12dc4ef1c33f5e350b5
Doub1e007 <3477848352@qq.com>
2025-10-10 17:54:30 +0800

    vue工程

---
+++ Vue工程/vue-project1/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import OptionView from './views/OptionView.vue'
+
+</script>
+
+<template>
+  <h1>Hello Vue...</h1>
+  <!-- 要展示什么组件就在这里写 -->
+  <option-view></option-view>
+</template>
+
+<style scoped> 
+
+</style>
---
+++ Vue工程/vue-project1/src/views/OptionView.vue
@@ -0,0 +1,34 @@
+<script>
+// 选项式API
+export default {
+    // 声明数据模型
+    data(){
+        return {
+            message: 'Hello 声明式API...',
+            count: 0
+        }
+    },
+
+    // 声明函数
+    methods:{
+        increment(){
+            this.count++;
+        }
+    },
+
+    // 声明钩子函数
+    mounted(){
+        console.log('页面组件挂载完成...');
+    },
+}
+</script>
+
+<template>
+    <h1>{{ message }}</h1>
+
+    <input type="button" value="点击+1" @click="increment"> {{ count }}
+</template>
+
+<style scoped>
+
+</style>
