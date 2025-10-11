7f8ae46ab9351c2cc33aeefcc27e2beceafcdb1b
Doub1e007 <3477848352@qq.com>
2025-10-12 01:08:45 +0800

    Vue工程化

--- Vue工程/vue-project1/src/App.vue
+++ Vue工程/vue-project1/src/App.vue
@@ -1,16 +1,19 @@
 <script setup>
 import OptionView from './views/OptionView.vue';
 import ConbineView from './views/ConbineView.vue';
+import UserList from './views/UserList.vue';
 
 
 </script>
 
 <template>
-  <h1>Hello Vue...</h1>
+  <!-- <h1>Hello Vue...</h1> -->
   <!-- 要展示什么组件就在这里写 -->
-  <option-view></option-view>
+  <!-- <option-view></option-view>
 
-  <ConbineView></ConbineView>
+  <ConbineView></ConbineView> -->
+
+  <user-list></user-list>
 </template>
 
 <style scoped> 
